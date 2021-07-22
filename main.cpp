6bb2b9a79b6f4c50c6b5e19423fb525ec86479bb
Sajeet Soren <sorsajeet@gmail.com>
2021-07-22 06:14:46 +0530

    master

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include "iostream"
 
 int main() {
+    std::cout << "this ho" << std::endl;
     std::cout << "this is just a git test" << std::endl;
+    std::cout << "this has to show" << std::endl;
     std::cout << "example htis is" << std::endl;
     return 0;
 }
