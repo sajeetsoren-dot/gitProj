071754d1bf544629020790f860cdadd037f69a46
Sajeet Soren <sorsajeet@gmail.com>
2021-07-22 06:18:13 +0530

    master

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "iostream"
 
 int main() {
+    std::cout << "this has to show itself" << std::cout;
     std::cout << "this ho" << std::endl;
     std::cout << "this is just a git test" << std::endl;
     std::cout << "this has to show" << std::endl;
