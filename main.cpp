c91f2a3b27b6e7a1033a1438d296258bb28c1bb1
Sajeet Soren <sorsajeet@gmail.com>
2021-07-22 08:29:11 +0530

    master

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "iostream"
 
 int main() {
+    std::cout << "syncronize" << std::cout;
     std::cout << "this has to be pushed" << std::endl;
     std::cout << "this has to show itself" << std::cout;
     std::cout << "this ho" << std::endl;
