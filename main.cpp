748f5b7b89ff774549227c100f3e6ebaa109c240
Sajeet Soren <sorsajeet@gmail.com>
2021-07-22 08:37:11 +0530

    master

--- main.cpp
+++ main.cpp
@@ -9,5 +9,6 @@ int main() {
     std::cout << "this is just a git test" << std::endl;
     std::cout << "this has to show" << std::endl;
     std::cout << "example htis is" << std::endl;
+    std::cout << "i am doing this for myself" << std::endl;
     return 0;
 }
